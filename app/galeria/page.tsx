ce8f35336853ac3b2f75d1a5f0d7c0ab2d2089e9
WektorZS <163024668+WektorZS@users.noreply.github.com>
2026-09-24 19:26:48 +0200

    g6

--- app/galeria/page.tsx
+++ app/galeria/page.tsx
@@ -54,18 +54,18 @@ export async function generateMetadata(): Promise<Metadata> {
 
 function getMosaicGroupClass(count: number) {
   if (count === 4) {
-    return "grid h-80 grid-cols-2 grid-rows-4 gap-0 sm:h-96 lg:h-96 lg:grid-cols-12 lg:grid-rows-4"
+    return "grid h-96 grid-cols-2 grid-rows-6 gap-0 sm:h-128 lg:h-96 lg:grid-cols-12 lg:grid-rows-4"
   }
 
   if (count === 3) {
-    return "grid h-64 grid-cols-2 grid-rows-3 gap-0 sm:h-72 lg:h-80 lg:grid-cols-12 lg:grid-rows-3"
+    return "grid h-80 grid-cols-2 grid-rows-4 gap-0 sm:h-96 lg:h-80 lg:grid-cols-12 lg:grid-rows-3"
   }
 
   if (count === 2) {
-    return "grid h-44 grid-cols-2 gap-0 sm:h-52 lg:h-56 lg:grid-cols-12"
+    return "grid h-52 grid-cols-2 grid-rows-1 gap-0 sm:h-72 lg:h-56 lg:grid-cols-12"
   }
 
-  return "grid h-48 grid-cols-1 gap-0 sm:h-56 lg:h-64 lg:grid-cols-12"
+  return "grid h-56 grid-cols-1 grid-rows-1 gap-0 sm:h-80 lg:h-64 lg:grid-cols-12"
 }
 
 function getMosaicTileClass(
@@ -75,10 +75,10 @@ function getMosaicTileClass(
 ) {
   if (count === 4) {
     const mobile = [
-      "col-start-1 col-span-1 row-start-1 row-span-2",
-      "col-start-2 col-span-1 row-start-1 row-span-1",
-      "col-start-2 col-span-1 row-start-2 row-span-1",
-      "col-start-1 col-span-2 row-start-3 row-span-2",
+      "col-start-1 col-span-2 row-start-1 row-span-2",
+      "col-start-1 col-span-1 row-start-3 row-span-2",
+      "col-start-2 col-span-1 row-start-3 row-span-2",
+      "col-start-1 col-span-2 row-start-5 row-span-2",
     ]
 
     const desktopDefault = [
@@ -104,9 +104,9 @@ function getMosaicTileClass(
 
   if (count === 3) {
     const mobile = [
-      "col-start-1 col-span-2 row-start-1 row-span-1",
-      "col-start-1 col-span-1 row-start-2 row-span-2",
-      "col-start-2 col-span-1 row-start-2 row-span-2",
+      "col-start-1 col-span-2 row-start-1 row-span-2",
+      "col-start-1 col-span-1 row-start-3 row-span-2",
+      "col-start-2 col-span-1 row-start-3 row-span-2",
     ]
 
     const desktopDefault = [
@@ -130,11 +130,11 @@ function getMosaicTileClass(
 
   if (count === 2) {
     return index === 0
-      ? "col-start-1 col-span-1 lg:col-start-1 lg:col-span-6"
-      : "col-start-2 col-span-1 lg:col-start-7 lg:col-span-6"
+      ? "col-start-1 col-span-1 row-start-1 lg:col-start-1 lg:col-span-6"
+      : "col-start-2 col-span-1 row-start-1 lg:col-start-7 lg:col-span-6"
   }
 
-  return "col-start-1 col-span-1 lg:col-span-12"
+  return "col-start-1 col-span-1 row-start-1 lg:col-span-12"
 }
 
 function getMosaicSizes(
@@ -143,7 +143,7 @@ function getMosaicSizes(
 ) {
   if (count === 4) {
     const sizes = [
-      "(max-width: 1023px) 50vw, 25vw",
+      "(max-width: 1023px) 100vw, 25vw",
       "(max-width: 1023px) 50vw, 33vw",
       "(max-width: 1023px) 50vw, 42vw",
       "(max-width: 1023px) 100vw, 75vw",
@@ -346,7 +346,7 @@ export default async function GalleryPage() {
 
         <section className="overflow-hidden bg-section-light pt-8 pb-4 sm:pt-12 sm:pb-0">
           {gallery.length ? (
-            <div className="mx-auto w-full max-w-screen-2xl px-4 sm:px-0">
+            <div className="mx-auto w-full px-4 sm:px-0 lg:max-w-5xl xl:max-w-6xl">
               <div className="overflow-hidden rounded-xl sm:rounded-none">
                 {galleryGroups.map(
                   (group, groupIndex) => {
